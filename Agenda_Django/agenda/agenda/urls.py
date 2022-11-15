652790718da7d03b10b1aa4a38c0ef8f01d9bf6e
Antonio Bigas <100382731+antoniobigas@users.noreply.github.com>
2022-11-14 23:20:56 -0300

    Adicionado funções de login e exceção 404
    Projeto final, adicionado sessões de login, demais paginas, exceção de erro..

--- Agenda_Django/agenda/agenda/urls.py
+++ Agenda_Django/agenda/agenda/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import RedirectView
+
+handler404 = views.handler404
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
+    path('agenda/lista/<int:id_usuario>/', views.json_lista_evento),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
+    path('agenda/historico/', views.lista_eventos_historico),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
